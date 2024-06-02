1ceef4812bb1cd668f4254884549c53b33f4f71b
grsfilippini <grsfilippini@gmail.com>
2024-06-02 14:29:39 -0300

    Atualizando versão do capistrano para 3.18.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock "~> 3.18.1"
 
 set :application, "inspecaovaso"
 set :repo_url, "git@github.com:grsfilippini/inspecao_vaso.git"
